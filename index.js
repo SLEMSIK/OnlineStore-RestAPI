11f42c52598a2f23f77130faf0a2273fe3016682
Vyacheslav Pechenov <84916321+SLEMSIK@users.noreply.github.com>
2025-07-30 13:27:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ import orderRoute from "./routes/orderRoute.js"
 const app = express ();
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 dotenv.config();
 
 const PORT = process.env.PORT || 80;
